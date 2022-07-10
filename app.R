83865e9f0247a96d2a118e403ac8b2955aa243f9
lodgepolepines <53026628+lodgepolepines@users.noreply.github.com>
2022-07-10 18:35:10 -0400

    add vzw data

--- app.R
+++ app.R
@@ -11,9 +11,7 @@ library(leaflegend)
 # register API key permanently with: register_google(key = "[your key]", write = TRUE)
 # for more information on geocoding: https://www.shanelynn.ie/massive-geocoding-with-r-and-google-maps/
 #sbux <- read.csv("starbucks_locations.csv", check.names = FALSE)
-#sbux <- sbux %>% 
-#mutate(location = paste0(Address, ", ", City, ", ", State))
-#sbux_geo <- mutate_geocode(sbux, location = location)
+#sbux_geo <- mutate_geocode(sbux, location = Address)
 #write.csv(sbux_geo, "sbux.csv")
 #mob <- read.csv("mobility_locations.csv", check.names = FALSE)
 #mob_geo <- mutate_geocode(mob, location = Name)
@@ -21,14 +19,22 @@ library(leaflegend)
 
 sbux <- read.csv("sbux.csv", check.names = FALSE)
 mob <- read.csv("mob.csv", check.names = FALSE)
+vzw <- read.csv("vzw_locations.csv", check.names = FALSE)
+vzw <- vzw %>% 
+  mutate(location = paste0(address, ", ", city, ", ", state))
 
 # convert points into sf object, then join with st_is_within_distance to find proximity locations
 close_mob <- st_as_sf(mob, coords = c("lon", "lat"))
 close_sbux <- st_as_sf(sbux, coords = c("lon", "lat"))
 distance <- set_units(.1) # specify maximum distance between Mobility and Starbucks locations
-combined<- st_join(close_mob, close_sbux, join=st_is_within_distance, dist = distance, left=FALSE)
-df <- distinct(combined, Name, .keep_all= TRUE)
-mob_df <- left_join(df, mob, by=c("Name" = "Name"))
+combined_mob <- st_join(close_mob, close_sbux, join=st_is_within_distance, dist = distance, left=FALSE)
+combined_mob_df <- distinct(combined_mob, Name, .keep_all= TRUE)
+mob_df <- left_join(combined_mob_df, mob, by=c("Name" = "Name"))
+
+close_vzw <- st_as_sf(vzw, coords = c("lon", "lat"))
+combined_vzw <- st_join(close_vzw, close_sbux, join=st_is_within_distance, dist = distance, left=FALSE)
+combined_vzw_df <- distinct(combined_vzw, store_name, .keep_all= TRUE)
+vzw_df <- left_join(combined_vzw_df, vzw, by=c("store_name" = "store_name"))
 
 # create icon set for legend
 iconSet <- awesomeIconList(
@@ -43,31 +49,44 @@ iconSet <- awesomeIconList(
     library = 'fa',
     iconColor = 'blue',
     markerColor = 'blue'
+  ),
+  VZW = makeAwesomeIcon(
+    icon = 'circle',
+    library = 'fa',
+    iconColor = 'red',
+    markerColor = 'red'
   )
 )
 # 
 
-# specify coffee icon design for Sbux locations
+# specify coffee icon design for Sbux location markers
 icons <- awesomeIcons(
   icon = 'coffee',
   iconColor = 'white',
   library = 'fa',
   markerColor = 'green')
 
-# create popup info for AT&T locations
-popup <- paste("<b>Title: </b>", 
-               sbux$Title,
-               "<br><b>Address: </b>", 
+# create popup info for Sbux locations
+popup <- paste("<b>Address: </b>", 
                sbux$Address,
-               "<br><b>City: </b>", 
-               sbux$City,
-               "<br><b>State: </b>", 
-               sbux$State,
                "<br><b>Status: </b>", 
-               sbux$Status) %>% 
+               sbux$Status,
+               "<br><b>Date Filed: </b>", 
+               sbux$"Date Filed",
+               "<br><b>Tally Date: </b>", 
+               sbux$"Tally Date",
+               "<br><b>Ballot Type: </b>",
+               sbux$"Ballot Type",
+               "<br><b>Votes For Union: </b>",
+               sbux$"Votes For Union",
+               "<br><b>Votes Against: </b>",
+               sbux$"Votes Against",
+               "<br><b>Total Eligible Voters: </b>",
+               sbux$"Num Eligible Voters"
+               ) %>% 
   lapply(htmltools::HTML)
 
-# create popup info for Sbux locations
+# create popup info for ATT locations
 popup2 <- paste("<b>Name: </b>", 
                 mob_df$Name,
                 "<br><b>District: </b>", 
@@ -80,9 +99,20 @@ popup2 <- paste("<b>Name: </b>",
                 mob_df$Workers.x) %>% 
   lapply(htmltools::HTML)
 
+# create popup info for VZW locations
+popup3 <- paste("<b>Name: </b>", 
+                vzw_df$store_name,
+                "<br><b>Address: </b>", 
+                vzw_df$location.x
+                #,
+                #"<br><b>Website: </b>",
+                #'<a href="', vzw_df$website_address.x, '">Link</a>'
+                ) %>% 
+  lapply(htmltools::HTML)
+
 # design overall dashboard user interface
 ui <- fluidPage(
-  HTML("<h3>CWA AT&T Mobility Locations + Starbucks Union Locations</h3>"),
+  HTML("<h3>CWA Wireless + Starbucks Union Locations</h3>"),
   mainPanel(
     leafletOutput("mymap", width="100%", height=1000)
   ))
@@ -110,7 +140,7 @@ server <- function(input, output, session) {
       setView(-104.96, 39.71, 4) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       addLayersControl(
-        overlayGroups = c("Starbucks", "AT&T"),
+        overlayGroups = c("Starbucks", "AT&T", "VZW"),
         options = layersControlOptions(collapsed = FALSE)
       ) %>%
       addLegendAwesomeIcon(iconSet = iconSet, # uses the icon set we designated earlier
@@ -128,6 +158,11 @@ server <- function(input, output, session) {
   observe({    
     leafletProxy("mymap", data=mob_df) %>%
       addCircleMarkers(~lon, ~lat, popup = popup2, group = "AT&T", color = 'blue', fillColor = 'blue', opacity = 0.5, radius = 5, fillOpacity = 0.5)
+  })
+  
+  observe({    
+    leafletProxy("mymap", data=vzw_df) %>%
+      addCircleMarkers(~lon, ~lat, popup = popup3, group = "VZW", color = 'red', fillColor = 'red', opacity = 0.5, radius = 5, fillOpacity = 0.5)
   })
   
   observe({    
